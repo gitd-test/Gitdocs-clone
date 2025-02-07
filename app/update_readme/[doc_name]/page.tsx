717c2872adf018ade8170656ced5f46b3f529678
Abhas Kumar Sinha <abhas.kumar22027@gmail.com>
2025-02-07 19:03:26 +0530

    next params - 2

--- app/update_readme/[doc_name]/page.tsx
+++ app/update_readme/[doc_name]/page.tsx
@@ -1,8 +1,10 @@
 import UpdateReadmePage from "@/components/Update_Readme/UpdateReadmePage";
 
 // Generate metadata for the page
-export const generateMetadata = ({ params }: { params: { doc_name: string } }) => {
-  const { doc_name } = params;
+export const generateMetadata = async ({ params }: { params: { doc_name: string } }) => {
+  const { doc_name } = await params;
+
+
 
   return {
     title: `Update Readme: ${doc_name} | Gitdocs AI`,
@@ -11,10 +13,9 @@ export const generateMetadata = ({ params }: { params: { doc_name: string } }) =
 };
 
 // Main component for the page
-const UpdateReadme = ({ params }: { params: { doc_name: string } }) => {
-  const { doc_name } = params;
+export default async function UpdateReadme({ params }: { params: { doc_name: string } }) {
+  const { doc_name } = await params;
+
 
   return <UpdateReadmePage doc_name={doc_name} />;
 };
-
-export default UpdateReadme;
