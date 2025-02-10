ffe6f3509d944923c50d5d40b1e4876e0334e3a5
Abhas Kumar Sinha <abhas.kumar22027@gmail.com>
2025-02-10 18:50:11 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import {
   ClerkProvider,
-} from '@clerk/nextjs'
-import './globals.css'
+} from '@clerk/nextjs';
+import Script from 'next/script';
+import './globals.css';
 import { Theme } from "@radix-ui/themes";
 import { AppProvider } from "@/contexts/AppContext";
 import SessionWrapper from "@/lib/SessionWrapper";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
@@ -26,16 +27,30 @@ export default function RootLayout({
             <meta name="bingbot" content="index, follow" />
             <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           </head>
-          <body style={{"backgroundColor" : "#0D0D0D"}}>
+          <body style={{ backgroundColor: "#0D0D0D" }}>
+            {/* Google Tag Manager */}
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-E0TKY71491"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-E0TKY71491');
+              `}
+            </Script>
+            {/* End Google Tag Manager */}
             <Theme>
-            <AppProvider>
-              {children}
-              <Analytics />
-            </AppProvider>
-          </Theme>
-        </body>
-      </html>
-    </ClerkProvider>
+              <AppProvider>
+                {children}
+                <Analytics />
+              </AppProvider>
+            </Theme>
+          </body>
+        </html>
+      </ClerkProvider>
     </SessionWrapper>
   )
 }
