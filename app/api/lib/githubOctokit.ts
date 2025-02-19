3184d755ce3b80fd80e880b9732931a12992c8d1
Abhas Kumar Sinha <abhas.kumar22027@gmail.com>
2025-02-19 18:37:54 +0530

    bug fixes - 2

--- app/api/lib/githubOctokit.ts
+++ app/api/lib/githubOctokit.ts
@@ -2,16 +2,21 @@ import { Octokit } from "@octokit/rest";
 import { createAppAuth } from "@octokit/auth-app";
 
 export const getAuthenticatedOctokit = (installationId: number) => {
-  const octokit = new Octokit({
-    authStrategy: createAppAuth,
-    auth: {
-      appId: process.env.GITHUB_APP_ID || "",
-      privateKey: process.env.GITHUB_PRIVATE_KEY || "",
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-      installationId: installationId,
-    },
-  });
+  try {
+    const octokit = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: process.env.GITHUB_APP_ID || "",
+        privateKey: process.env.GITHUB_PRIVATE_KEY || "",
+        clientId: process.env.GITHUB_CLIENT_ID || "",
+        clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+        installationId,
+      },
+    });
 
-  return octokit;
+    return octokit;
+  } catch (error) {
+    console.error("Error creating Octokit instance:", error);
+    throw new Error("Failed to authenticate Octokit");
+  }
 };
