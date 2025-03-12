659aa2c42214412689b99c2f51d652496a80db90
Abhas Kumar Sinha <abhas.kumar22027@gmail.com>
2025-03-12 21:26:44 +0530

    auto commit added - 3

--- app/api/auth/user/updateUserDb.ts
+++ app/api/auth/user/updateUserDb.ts
@@ -4,7 +4,7 @@ export const updateUserDb = async (user_id: string, github_user_id: string, inst
 
     // Update only if githubUid is empty (null or undefined)
     await User.findOneAndUpdate(
-        { clerkUid: user_id, githubUid: { $exists: false } }, // Check if githubUid doesn't exist
+        { clerkUid: user_id }, // Check if githubUid doesn't exist
         { $set: { githubUid: github_user_id, installationId: installation_id || null, githubUsername: github_username } }, // Update the githubUid field
         { new: true } // Return the updated document
     );
